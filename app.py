e2422c259b3b976000a8bde34b41a4c20c978bd7
babis <theodoroucharalabos@gmail.com>
2018-06-25 19:42:58 +0300

    Finish delete and put

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authendication, identify
 
@@ -16,6 +16,12 @@ jwt = JWT(app, authendication, identify) # This will create a new end point /aut
 # This is a resource
 
 class Item(Resource):
+    # parsing the request using reqparser
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float,
+                        required=True,
+                        help='This field cannot be blank')
+
     # @app.route('student/<string:name>') We can do it that way or user our api and add resources to it
     @jwt_required()
     def get(self, name):
@@ -23,16 +29,37 @@ class Item(Resource):
 
         return {"item": item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, name):
+
         if next(filter(lambda x: x["name"] == name, items), None) is not None:
             return {"message": "An item with name {} already exists".format(name)}, 400 # 400 bad request
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {
             "name": name,
             "price": data["price"]
         }
+
         items.append(item)
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+
+    @jwt_required()
+    def put(self, name):
+
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] != name, items), None)
+        if item is None:
+            item = {"name": name, "price": data['price']}
+            items.append(item)
+        else:
+            item.update(data)
         return item
 
 
